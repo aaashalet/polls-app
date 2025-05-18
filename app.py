0560eb4c835c8dc86085dd42c3dbd970c48a9444
ashalet <soulboy228@gmail.com>
2025-05-19 02:34:10 +0700

    Final

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
+import requests
 from flask import Flask, render_template, redirect, url_for, flash, request, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user, UserMixin
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from werkzeug.utils import secure_filename
-from wtforms import StringField, PasswordField, SubmitField, FieldList
+from wtforms import StringField, PasswordField, SubmitField, FieldList, BooleanField
 from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
 from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
@@ -19,6 +20,7 @@ app.config['SECRET_KEY'] = 'your_secret_key_here'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///polls.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['OWM_API_KEY'] = '25d075fe6486da186e37a03813a8e932'
 
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
@@ -36,6 +38,7 @@ class User(UserMixin, db.Model):
     is_admin = db.Column(db.Boolean, default=False)
     avatar_filename = db.Column(db.String(300), nullable=True)
     votes = db.relationship('Vote', backref='user', lazy=True)
+    polls_created = db.relationship('Poll', backref='creator', lazy=True)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -48,6 +51,7 @@ class Poll(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     question = db.Column(db.String(500), nullable=False)
     image_filename = db.Column(db.String(300))
+    creator_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     options = db.relationship('Option', backref='poll', lazy=True)
     votes = db.relationship('Vote', backref='poll', lazy=True)
 
@@ -78,6 +82,21 @@ def admin_required(f):
             abort(403)
         return f(*args, **kwargs)
     return decorated_function
+
+
+class UserEditForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=150)])
+    is_admin = BooleanField('Administrator')
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user and str(user.id) != self.user_id:
+            raise ValidationError('Username already exists.')
+
+    def __init__(self, user_id=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user_id = user_id
 
 
 class RegistrationForm(FlaskForm):
@@ -143,8 +162,12 @@ def create_poll_chart(poll):
 
 @app.route('/')
 def index():
-    polls = Poll.query.all()
-    return render_template('index.html', polls=polls)
+    query = request.args.get('q', '')
+    if query:
+        polls = Poll.query.filter(Poll.question.ilike(f'%{query}%')).all()
+    else:
+        polls = Poll.query.all()
+    return render_template('index.html', polls=polls, query=query)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -241,7 +264,7 @@ def create_poll():
                     flash('Invalid image format.', 'danger')
                     return redirect(request.url)
 
-            poll = Poll(question=form.question.data, image_filename=filename)
+            poll = Poll(question=form.question.data, image_filename=filename, creator_id=current_user.id)
             db.session.add(poll)
             db.session.commit()
             for option_text in form.options.data:
@@ -274,6 +297,7 @@ def poll_detail(poll_id):
             return redirect(url_for('poll_detail', poll_id=poll_id))
         else:
             flash('Invalid option.', 'danger')
+
     votes_count = {option.id: len(option.votes) for option in poll.options}
     total_votes = sum(votes_count.values())
 
@@ -281,7 +305,94 @@ def poll_detail(poll_id):
     if voted:
         chart_filename = create_poll_chart(poll)
 
-    return render_template('poll_detail.html', poll=poll, voted=voted, votes_count=votes_count, total_votes=total_votes, chart_filename=chart_filename)
+    return render_template('poll_detail.html', poll=poll, voted=voted,
+                           votes_count=votes_count, total_votes=total_votes, chart_filename=chart_filename)
+
+
+@app.route('/poll/<int:poll_id>/edit', methods=['GET', 'POST'])
+@login_required
+def edit_poll(poll_id):
+    poll = Poll.query.get_or_404(poll_id)
+
+    if poll.creator_id != current_user.id and not current_user.is_admin:
+        abort(403)
+
+    form = PollForm(obj=poll)
+    if request.method == 'GET':
+        if len(form.options) < len(poll.options):
+            for _ in range(len(poll.options) - len(form.options)):
+                form.options.append_entry()
+        for i, option in enumerate(poll.options):
+            form.options[i].data = option.text
+
+    if request.method == 'POST':
+        if 'add_option' in request.form:
+            form.options.append_entry()
+            return render_template('edit_poll.html', form=form, poll=poll)
+        if form.validate_on_submit():
+            poll.question = form.question.data
+
+            if form.image.data:
+                file = form.image.data
+                if allowed_file(file.filename):
+                    filename = secure_filename(file.filename)
+                    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                    poll.image_filename = filename
+                else:
+                    flash('Invalid image format.', 'danger')
+                    return redirect(request.url)
+
+            Option.query.filter_by(poll_id=poll.id).delete()
+            for option_text in form.options.data:
+                option = Option(text=option_text, poll_id=poll.id)
+                db.session.add(option)
+
+            db.session.commit()
+            flash('Poll updated successfully.')
+            return redirect(url_for('poll_detail', poll_id=poll.id))
+
+    return render_template('edit_poll.html', form=form, poll=poll)
+
+
+@app.route('/poll/<int:poll_id>/delete', methods=['POST'])
+@login_required
+def delete_poll(poll_id):
+    poll = Poll.query.get_or_404(poll_id)
+    if poll.creator_id != current_user.id and not current_user.is_admin:
+        abort(403)
+    Vote.query.filter_by(poll_id=poll.id).delete()
+    Option.query.filter_by(poll_id=poll.id).delete()
+    db.session.delete(poll)
+    db.session.commit()
+    flash('Poll deleted.', 'success')
+    return redirect(url_for('index'))
+
+
+@app.route('/weather', methods=['GET', 'POST'])
+def weather():
+    weather_data = None
+    error = None
+
+    if request.method == 'POST':
+        city = request.form.get('city')
+        if city:
+            api_key = app.config['OWM_API_KEY']
+            url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric&lang=en'
+            response = requests.get(url)
+            if response.status_code == 200:
+                data = response.json()
+                weather_data = {
+                    'city': data['name'],
+                    'temp': data['main']['temp'],
+                    'description': data['weather'][0]['description'].capitalize(),
+                    'icon': data['weather'][0]['icon']
+                }
+            else:
+                error = 'City not found or API error.'
+        else:
+            error = 'Please enter a city name.'
+
+    return render_template('weather.html', weather=weather_data, error=error)
 
 
 @app.route('/admin')
@@ -290,11 +401,56 @@ def admin_index():
     return render_template('admin/index.html')
 
 
+@app.route('/admin/users')
+@admin_required
+def admin_users():
+    page = request.args.get('page', 1, type=int)
+    search = request.args.get('search', '', type=str)
+    query = User.query
+    if search:
+        query = query.filter(User.username.ilike(f'%{search}%'))
+    pagination = query.order_by(User.username).paginate(page=page, per_page=20)
+    return render_template('admin/users.html', users=pagination, search=search)
+
+
+@app.route('/admin/users/edit/<int:user_id>', methods=['GET', 'POST'])
+@admin_required
+def admin_edit_user(user_id):
+    user = User.query.get_or_404(user_id)
+    form = UserEditForm(user_id=str(user.id), obj=user)
+    if form.validate_on_submit():
+        user.username = form.username.data
+        user.is_admin = form.is_admin.data
+        db.session.commit()
+        flash('User updated successfully.', 'success')
+        return redirect(url_for('admin_users'))
+    return render_template('admin/edit_user.html', form=form, user=user)
+
+
+@app.route('/admin/users/delete/<int:user_id>', methods=['POST'])
+@admin_required
+def admin_delete_user(user_id):
+    user = User.query.get_or_404(user_id)
+    if user.id == current_user.id:
+        flash("You cannot delete yourself!", "danger")
+        return redirect(url_for('admin_users'))
+    Vote.query.filter_by(user_id=user.id).delete()
+    db.session.delete(user)
+    db.session.commit()
+    flash('User deleted.', 'success')
+    return redirect(url_for('admin_users'))
+
+
 @app.route('/admin/polls')
 @admin_required
 def admin_polls():
-    polls = Poll.query.all()
-    return render_template('admin/polls.html', polls=polls)
+    page = request.args.get('page', 1, type=int)
+    search = request.args.get('search', '', type=str)
+    query = Poll.query
+    if search:
+        query = query.filter(Poll.question.ilike(f'%{search}%'))
+    pagination = query.order_by(Poll.id.desc()).paginate(page=page, per_page=10)
+    return render_template('admin/polls.html', polls=pagination, search=search)
 
 
 @app.route('/admin/polls/delete/<int:poll_id>', methods=['POST'])
@@ -307,24 +463,6 @@ def admin_delete_poll(poll_id):
     db.session.commit()
     flash('Poll deleted.', 'success')
     return redirect(url_for('admin_polls'))
-
-
-@app.route('/admin/users')
-@admin_required
-def admin_users():
-    users = User.query.all()
-    return render_template('admin/users.html', users=users)
-
-
-@app.route('/admin/users/delete/<int:user_id>', methods=['POST'])
-@admin_required
-def admin_delete_user(user_id):
-    user = User.query.get_or_404(user_id)
-    Vote.query.filter_by(user_id=user.id).delete()
-    db.session.delete(user)
-    db.session.commit()
-    flash('User deleted.', 'success')
-    return redirect(url_for('admin_users'))
 
 
 if __name__ == '__main__':
